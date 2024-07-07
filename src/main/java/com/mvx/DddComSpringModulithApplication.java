28f5a7184f31d3674912fe8176a4d2e834c6b114
Marcus Almeida <marcusmage@gmail.com>
2024-07-07 15:17:50 -0300

    commit inicial

---
+++ src/main/java/com/mvx/DddComSpringModulithApplication.java
@@ -0,0 +1,13 @@
+package com.mvx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddComSpringModulithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddComSpringModulithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvx/DddComSpringModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddComSpringModulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
